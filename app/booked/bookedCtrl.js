179ba8e7240117ff0d34016edae392f5a01b3b14
Nick Hathaway <Nicholashathaway@gmail.com>
2017-11-15 13:15:06 -0700

    step 8

--- app/booked/bookedCtrl.js
+++ app/booked/bookedCtrl.js
@@ -1,2 +1,10 @@
-angular.module('devmtnTravel').controller('bookedCtrl', function($scope) {
+angular.module('devmtnTravel').controller('bookedCtrl', function($scope, $stateParams, mainSrvc) {
+  mainSrvc.getPackageInfo().then(res => {
+    $scope.allPackages = res.data;
+    $scope.allPackages.forEach(package => {
+      if (package.id && (package.id == $stateParams.id)) {
+        $scope.package = package;
+      }
+    }) 
+  })
 })
