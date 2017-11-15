a09af5d778a71a30b9c984a987d05a3d5d1b19b0
Nick Hathaway <Nicholashathaway@gmail.com>
2017-11-15 13:19:08 -0700

    finished all steps

--- app/packages/packagesCtrl.js
+++ app/packages/packagesCtrl.js
@@ -1,2 +1,8 @@
-angular.module('devmtnTravel').controller('packagesCtrl', function($scope) {
+angular.module('devmtnTravel').controller('packagesCtrl', function($scope, $stateParams, mainSrvc) {
+  mainSrvc.getPackageInfo().then(res => {
+    $scope.allPackages = res.data;
+    $scope.packages = $scope.allPackages.filter(package => {
+      return package.country === $stateParams.country;
+    })
+  })
 })
